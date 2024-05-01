dfafe5083bdca76e7f0ba5e851f9256888311ab1
twnchk <facebaster@gmail.com>
2024-05-01 20:10:03 +0200

    Add environs, modify docker-compose and add templates for password reset

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+env = Env()
+env.read_env()  # read .env file if it exists
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x+ultdl^cdw6wq5xq208=)#*a@uz3qdbc5-til+h3&m2y=*5)1'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
